74a0a1f7cfbd193cc28c5d535d7877ac8f480ed3
Yohan-Ralambo <yohanralambo@gmail.com>
2025-09-03 12:15:41 +0300

    examen

--- main.py
+++ main.py
@@ -1,7 +1,43 @@
+from http.client import HTTPException
+from typing import List
+
 from fastapi import FastAPI
-from starlette.responses import Response, FileResponse, PlainTextResponse
+from pydantic import BaseModel
+from pyexpat import model
+from starlette.responses import Response, FileResponse, PlainTextResponse, JSONResponse
+
 app = FastAPI()
+
+
+class post(BaseModel):
+    identifier: str
+    brand: str
+    model: str
+    characteristics: object
+
+post_stored: List[post] = []
+
 
 @app.get("/ping")
 def ping():
-    return  FileResponse ("pong")
+    return FileResponse("pong")
+
+
+@app.post("/cars")
+def create_post(post_payload: List[post]):
+    post_stored.extend(post_payload)
+    return JSONResponse(content=post, status_code=201, media_type="application/json")
+
+
+@app.get("/cars")
+def list_posts():
+    return JSONResponse(content=post_stored, status_code=200, media_type="application/json")
+
+
+@app.get("/cars/{identifier}")
+def read_car(identifier: str):
+    for post in post_stored:
+        if post.identifier == identifier:
+            return JSONResponse(content=post, status_code=200, media_type="application/json")
+
+        raise HTTPException(status_code=404, detail="Car not found")
